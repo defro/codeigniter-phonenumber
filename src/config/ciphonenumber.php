a3a8af65ceaf8cba68ad6b2e88627b80910d45a9
Joël Gaujard <j.gaujard@gmail.com>
2016-12-25 05:58:22 +0100

    Create config file

---
+++ src/config/ciphonenumber.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| National Mode - Type: Boolean
+|--------------------------------------------------------------------------
+|
+| Allow users to enter national numbers (and not have to think about
+| international dial codes).
+| This option now defaults to true, and it is recommended that you
+| leave it that way as it provides a better experience for the user.
+|
+*/
+$config['ciphonenumber_nationalMode'] = TRUE;
+
