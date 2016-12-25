7f7788f68102573ec3cda5238b1cd2ab9890a3cc
Joël Gaujard <j.gaujard@gmail.com>
2016-12-25 03:13:57 +0100

    Create a view file for the Example

--- src/controllers/Example.php
+++ src/controllers/Example.php
@@ -1,11 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Example extends MY_Controller
+class Example extends CI_Controller
 {
 
 	public function index()
 	{
+		//ptr(FCPATH . 'vendor/defro/codeigniter-phonenumber/src', 1);
+		$this->load->library('LibPhoneNumber');
+		//$phone = $this->libphonenumber->parse('0658831904', 'FR');
+		//$isValid = $this->libphonenumber->isValidNumber($phone);
+		//ptr( $this );
+
+		$data = array();
+		$this->load->view('example', $data);
 	}
 
 }
