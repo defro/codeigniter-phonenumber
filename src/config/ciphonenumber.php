f239bacd186f856879c14088b123dc0d3855edec
Joël Gaujard <j.gaujard@gmail.com>
2016-12-27 13:47:38 +0100

    Add a configuration variable : path to intl-tel-input ressources

--- src/config/ciphonenumber.php
+++ src/config/ciphonenumber.php
@@ -3,6 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
+| Path to intl-tel-input plugin
+|--------------------------------------------------------------------------
+|
+| Path to Intl Tel Input medias includes css, img and js folders.
+| This option is already set to right folder when you install
+| this library with composer.
+|
+*/
+$config['ciphonenumber_intltelinput_path'] = 'vendor/jackocnr/intl-tel-input/build/';
+
+/*
+|--------------------------------------------------------------------------
 | National Mode - Type: Boolean
 |--------------------------------------------------------------------------
 |
